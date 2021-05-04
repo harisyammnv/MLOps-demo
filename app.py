9eb0c2f6c32b9c3f7ce00c0519c94a4c78a2bf37
harisyammnv <harisyam.bphc@gmail.com>
2021-05-04 22:07:45 +0200

    host removed

--- app.py
+++ app.py
@@ -35,4 +35,5 @@ def index():
         return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run()

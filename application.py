7d33535308341d3630e1b59ee3cc4cf4a6fe78d7
harisyammnv <harisyam.bphc@gmail.com>
2021-05-04 22:17:46 +0200

    app chnaged to application for eb

--- application.py
+++ application.py
@@ -9,10 +9,10 @@ webapp_root = "webapp"
 static_dir = os.path.join(webapp_root, "static")
 template_dir = os.path.join(webapp_root, "templates")
 
-app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
+application = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
 
 
-@app.route("/", methods=["GET", "POST"])
+@application.route("/", methods=["GET", "POST"])
 def index():
 
     if request.method == "POST":
@@ -36,4 +36,4 @@ def index():
 
 if __name__ == "__main__":
     #app.run(host='0.0.0.0', port=5000, debug=True)
-    app.run()
+    application.run()
